6ea1d35b1356f9966dff363595b654cdf5b92bde
Morse <enmorse48@gmail.com>
2021-09-23 23:50:54 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+# Make a list of the numbers from one to one million,
+# and then use a for loop to print the numbers. (If the
+# output is taking too long, stop it by pressing
+# CTRL - C or by closing the output window.)
+
+one_million = []
+
+for value in range(1, 1000001):
+    one_million.append(value)
+
+print(one_million)
